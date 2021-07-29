b7d316686073501e1d2a3cfb737450ae4aaa48b1
jimyds <danilocarvalho.ep@gmail.com>
2021-07-28 22:28:34 -0300

    chore: add cors config

---
+++ src/main/java/br/com/danilo/seniorchallenge/conf/CorsConfiguration.java
@@ -0,0 +1,27 @@
+package br.com.danilo.seniorchallenge.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsConfiguration implements Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,PUT,OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Max-Age", "86400");
+        chain.doFilter(req, res);
+    }
+}
